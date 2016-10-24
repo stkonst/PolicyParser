f4c5139fba4e65048b40504566736aa5199aaa0d
Stavros Konstantaras <stavros@nlnetlabs.nl>
2016-10-24 15:28:23 +0200

    Introducing YAML output.

--- librpsl.py
+++ librpsl.py
@@ -6,9 +6,10 @@ import parsers
 import resolvers
 import rpsl
 import xmlGenerator
+import yamlGenerator
 
 
-def build_XML_policy(autnum, ipv6=True, output='screen', black_list=set()):
+def build_policy(autnum, ipv6=True, output='screen', black_list=set(), policy_format="XML"):
     #
     # PreProcess section: Get own policy, parse and create necessary Data
     #                     Structures.
@@ -33,25 +34,35 @@ def build_XML_policy(autnum, ipv6=True, output='screen', black_list=set()):
     fr.resolve_filters()
 
     #
-    # PostProcess section: Create and deliver the corresponding XML output.
+    # PostProcess section: Create and deliver the corresponding output.
     #
-    xmlgen = xmlGenerator.XmlGenerator(autnum)
-    xmlgen.convert_peers_to_XML(pp.peerings)
-    xmlgen.convert_filters_to_XML(pp.filter_expressions)
-    xmlgen.convert_lists_to_XML(fr.AS_list, fr.AS_dir, fr.RS_list, fr.RS_dir,
-                                fr.AS_set_list, fr.AS_set_dir)
+    if policy_format == "YAML":
+        yo = yamlGenerator.YamlGenerator()
+        yo.convert_lists_to_dict(fr.AS_list, fr.AS_dir, fr.RS_list, fr.RS_dir,
+                                 fr.AS_set_list, fr.AS_set_dir)
+        yo.convert_filters_to_dict(pp.filter_expressions)
+        yo.convert_peers_to_yaml(pp.peerings)
+        return yo.print_policy()
 
-    if output == "browser":
-        return str(xmlgen)
-    elif output == "screen":
-        reparsed = parseString(str(xmlgen))
-        return reparsed.toprettyxml(indent="\t")
-    elif output == "file":
-        return str(xmlgen)
+    else:
+        xmlgen = xmlGenerator.XmlGenerator(autnum)
+        xmlgen.convert_peers_to_XML(pp.peerings)
+        xmlgen.convert_filters_to_XML(pp.filter_expressions)
+        xmlgen.convert_lists_to_XML(fr.AS_list, fr.AS_dir, fr.RS_list, fr.RS_dir,
+                                    fr.AS_set_list, fr.AS_set_dir)
+
+        if output == "browser":
+            return str(xmlgen)
+        elif output == "screen":
+            reparsed = parseString(str(xmlgen))
+            return reparsed.toprettyxml(indent="\t")
+        elif output == "file":
+            return str(xmlgen)
 
 
 if __name__ == "__main__":
     import argparse
+
 
     def read_ASN(string):
         if not rpsl.is_ASN(string):
@@ -60,10 +71,10 @@ if __name__ == "__main__":
                                              .format(string))
         return string
 
+
     def read_file(string):
-        if not string.endswith('.xml'):
-            string += '.xml'
-        return string
+        return string.lower()
+
 
     def read_blacklisted(string):
         items = set(string.split(","))
@@ -74,11 +85,19 @@ if __name__ == "__main__":
                                                  .format(i))
         return items
 
+
+    def read_format(string):
+        if string in ['YAML', 'yaml']:
+            return "YAML"
+        else:
+            return "XML"
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument('ASN', type=read_ASN,
                         help="Autonomous System name in AS<number> format.")
     parser.add_argument('-o', '--outputfile', type=read_file,
-                        help="Name of the XML file produced. An additional "
+                        help="Name of the output file produced. An additional "
                              ".log file will also be created.")
     parser.add_argument('-b', '--blacklist', type=read_blacklisted,
                         help="A comma separated list of AS numbers that can "
@@ -86,6 +105,8 @@ if __name__ == "__main__":
                         default=set())
     parser.add_argument('-d', '--debug', action="store_true",
                         help="Log additional debugging information.")
+    parser.add_argument('-f', '--format', type=read_format,
+                        help="Select the format of the output file. It can be either XML (default) or YAML.")
 
     args = parser.parse_args()
     if args.debug:
@@ -96,25 +117,32 @@ if __name__ == "__main__":
 
     logging.getLogger("requests").setLevel(logging.WARNING)
     if args.outputfile:
+
+        if not args.outputfile.endswith('.xml') or not args.outputfile.endswith('.yaml'):
+            if args.format == "YAML":
+                args.outputfile += '.yaml'
+            else:
+                args.outputfile += '.xml'
+
         logging.basicConfig(filename=args.outputfile + '.log', filemode="w",
                             level=logging_level)
-        xml_result = build_XML_policy(args.ASN, output='file',
-                                      black_list=args.blacklist)
-        if xml_result:
+        policy_result = build_policy(args.ASN, output='file',
+                                     black_list=args.blacklist, policy_format=args.format)
+        if policy_result:
             with open(args.outputfile, mode='w') as f:
-                f.write(xml_result)
+                f.write(policy_result)
     else:
         logging.basicConfig(level=logging_level)
-        xml_result = build_XML_policy(args.ASN, output='screen',
-                                      black_list=args.blacklist)
-        if xml_result:
-            print(xml_result)
+        policy_result = build_policy(args.ASN, output='screen',
+                                     black_list=args.blacklist, policy_format=args.format)
+        if policy_result:
+            print(policy_result)
 
-    if xml_result:
-        logging.info("All done. XML policy is ready.")
-        print("All done. XML policy is ready.")
+    if policy_result:
+        logging.info("All done. Converted policy is ready.")
+        print("All done. Converted policy is ready.")
     else:
-        logging.info("XML policy was not created. Check the logs for more "
+        logging.info("Converted policy was not created. Check the logs for more "
                      "information.")
-        print("XML policy was not created. Check the logs for more "
+        print("Converted policy was not created. Check the logs for more "
               "information.")
---
+++ yamlGenerator.py
@@ -0,0 +1,192 @@
+import datetime
+from collections import deque
+
+import yaml
+
+
+class YamlGenerator:
+    def __init__(self):
+        self.yaml_policy = {'datetime': str(datetime.datetime.now())}
+
+    def print_policy(self):
+        return yaml.dump(self.yaml_policy, default_flow_style=False, allow_unicode=True, indent=4)
+
+    def _get_all_AS_routes(self, AS_object, routes4, routes6):
+        """ Returns all the prefixes of a given AS object in two different sets (IPv4 and IPv6)
+        """
+
+        for r in AS_object.route_obj_dir.origin_table.itervalues():
+            routes4.append(r.route)
+
+        for r in AS_object.route_obj_dir.origin_table_v6.itervalues():
+            routes6.append(r.route)
+
+    def _convert_AS_to_dict(self, AS_object):
+        """Converts a given AS number into a dictionary which includes all IPv4 and IPv6 addresses
+       """
+
+        routes4 = []
+        routes6 = []
+
+        self._get_all_AS_routes(AS_object, routes4, routes6)
+
+        return {AS_object.origin: {'ipv4': routes4, 'ipv6': routes6}}
+
+    def _convert_ASset_to_dict(self, AS_set_object, AS_object_dir, AS_set_object_dir):
+        """Traverses the tree created by the AS sets. Ignores duplicate ASes
+        and avoids loops.
+        """
+        AS_set_tree = deque([AS_set_object.get_key()])
+        traversed_AS_sets = set()
+        traversed_ASes = set()
+
+        routes4 = []
+        routes6 = []
+
+        while AS_set_tree:
+            current_set_name = AS_set_tree.popleft()
+            current_set = AS_set_object_dir.data[current_set_name]
+            for child_AS in current_set.ASN_members:
+                if child_AS not in traversed_ASes:
+                    traversed_ASes.add(child_AS)
+                    try:
+                        self._get_all_AS_routes(AS_object_dir.data[child_AS], routes4, routes6)
+                    except KeyError:
+                        pass
+
+            for child_set in current_set.AS_set_members:
+                if child_set not in traversed_AS_sets:
+                    traversed_AS_sets.add(child_set)
+                    AS_set_tree.extend(child_set)
+
+        return {'ipv4': routes4, 'ipv6': routes6}
+
+    def _convert_RS_to_dict(self, route_set_object, route_set_object_dir):
+        """Traverses the tree created by the RSes. Ignores duplicate routes
+        and avoids loops.
+        """
+        route_set_tree = deque([route_set_object.get_key()])
+        traversed_route_sets = set()
+        traversed_routes = set()
+
+        routes4 = []
+        routes6 = []
+
+        while route_set_tree:
+            current_set_name = route_set_tree.popleft()
+            current_set = route_set_object_dir.data[current_set_name]
+            for r in current_set.members.origin_table.itervalues():
+                if r.route not in traversed_routes:
+                    routes4.append(r.route)
+                    traversed_routes.add(r.route)
+
+            for r in current_set.mp_members.origin_table_v6.itervalues():
+                if r.route not in traversed_routes:
+                    routes6.append(r.route)
+                    traversed_routes.add(r.route)
+
+            for child_set in current_set.RSes_dir:
+                if child_set not in traversed_route_sets:
+                    traversed_route_sets.add(child_set)
+                    route_set_tree.extend(child_set)
+
+        return {'ipv4': routes4, 'ipv6': routes6}
+
+    def _filter_to_dict(self, peer_filter):
+
+        statements = {}
+        for i, t in enumerate(peer_filter.statements):
+
+            statement = {}
+            if t.allow:
+                statement['type'] = 'accept'
+            else:
+                statement['type'] = 'deny'
+
+            for item in t.members:
+                if item.category == "AS_PATH":
+                    statement['AS_PATH'] = item.data[1]
+                elif item.category in ("AS", "AS_set", "rs_set"):
+                    statement['prefix-list'] = str(item.data)
+                elif item.category == "prefix_list":
+                    statement['prefix-list'] = [p for p in item.data]
+
+            statements[str(i)] = statement
+        return {peer_filter.hash_value: {'afi': peer_filter.afi, 'expression': peer_filter.expression,
+                                         'statements': statements}}
+
+    def _peer_to_dict(self, peer_AS):
+
+        # ppoints= []
+        # for pp in peer_AS.peering_points.itervalues():
+
+        # actions_in = {}
+        # actions_out = {}
+        # ppoints.append({"local-IP": pp.local_ip, "remote-IP": pp.remote_ip,
+        #                 "actions_in": actions_in, "actions_out": actions_out})
+
+        # return {peer_AS.origin: {"peering-points": ppoints, "filters": {"imports": [], "exports": [],
+        #                                                                 "mp-imports": [], "mp-exports": []}}}
+        imports = []
+        exports = []
+
+        for f, v in peer_AS.filters.iteritems():
+            if v == "import":
+                imports.append(f)
+
+            elif v == "export":
+                exports.append(f)
+
+        mp_imports = []
+        mp_exports = []
+
+        for f, v in peer_AS.mp_filters.iteritems():
+            if v == "import":
+                mp_imports.append(f)
+
+            elif v == "export":
+                mp_exports.append(f)
+
+        return {peer_AS.origin: {"filters": {"imports": imports, "exports": exports, "mp-imports": mp_imports,
+                                             "mp-exports": mp_exports}}}
+
+    def convert_lists_to_dict(self, AS_list, AS_object_dir, RS_list, route_set_object_dir, AS_set_list,
+                              AS_set_object_dir):
+        dic_objects = dict()
+        for s in AS_set_list:
+            try:
+                obj = AS_set_object_dir.data[s]
+                dic_objects[s] = self._convert_ASset_to_dict(obj, AS_object_dir, AS_set_object_dir)
+            except KeyError:
+                pass
+
+        for v in AS_list:
+            try:
+                obj = AS_object_dir.data[v]
+                dic_objects.update(self._convert_AS_to_dict(obj))
+
+            except KeyError:
+                pass
+
+        for r in RS_list:
+            try:
+                obj = route_set_object_dir.data[r]
+                dic_objects[r] = self._convert_RS_to_dict(obj, route_set_object_dir)
+            except KeyError:
+                pass
+
+        self.yaml_policy['prefix-lists'] = dic_objects
+
+    def convert_filters_to_dict(self, peer_filter_dir):
+        pfilters = {}
+        for val in peer_filter_dir.filter_table.itervalues():
+            pfilters.update(self._filter_to_dict(val))
+
+        self.yaml_policy['peering-filters'] = pfilters
+
+    def convert_peers_to_yaml(self, peer_obj_dir):
+        peersdir = {}
+
+        for val in peer_obj_dir.peer_table.itervalues():
+            peersdir.update(self._peer_to_dict(val))
+        self.yaml_policy['peering-policy'] = peersdir
